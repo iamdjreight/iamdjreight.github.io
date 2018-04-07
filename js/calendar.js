a7a672372fd75c2a3a24d76942b8552424c16884
iamdjreight <32808968+iamdjreight@users.noreply.github.com>
2018-04-06 19:24:54 -0700

    Update calendar.js
    updated calendar.js to auto-insert Havana House events

--- js/calendar.js
+++ js/calendar.js
@@ -1,18 +1,52 @@
-// let date = '4/1/18';
-// let venue = 'Yo Momma\'s house';
-// let time = 'All the time';
+// Vars
+const today = new Date(),
+    date = today.getDate(),
+    dayOfWeek = today.getDay(),
+    daysFromThursday = dayOfWeek - 4,
+    calendarArray = [];
 
-// document.querySelector('.djevent').innerHTML = `
-// <td>${date}</td>
-// <td>${venue}</td>
-// <td>${time}</td>
-// `;
+let thursday = new Date(today.setDate(date - daysFromThursday)),
+    thursdayEpoch,
+    weeks = 0;
 
-////////HTML include import code////////////////
+// Create list of Thursdays
+while (weeks < 10){
+  thursdayEpoch = thursday.getTime();  
+  calendarArray.push(thursdayEpoch);
+  thursday = new Date(thursday.setDate(thursday.getDate() + 7));
+  weeks++;
+}
 
-let link = document.querySelector('link[rel="import"]');
-let calendar = link.import;
-let content = calendar.querySelector('table');
-let calendarDiv = document.querySelector('.calendar-div');
+// Create Calendar
+let tableBody = document.querySelector('tbody'),
+    row = document.createElement('tr'),
+    data = document.createElement('td');
 
-calendarDiv.appendChild(content.cloneNode(true));
+// calendarArray.forEach(thurs => console.log(new Date(thurs)));
+
+calendarArray.forEach(function (thurs){
+  if(thurs > new Date().getTime()){ 
+    const tableBody = document.querySelector('tbody'),
+          tableRow = document.createElement('tr'),
+          tableData = document.createElement('td'),
+          tableData2 = document.createElement('td'),
+          tableData3 = document.createElement('td');
+          
+    let month = new Date(thurs).getMonth() + 1,
+        day = new Date(thurs).getDate(),
+        year = new Date(thurs).getFullYear().toString().substring(2),
+        tdText = `${month}/${day}/${year}`;
+
+    tableData.appendChild(document.createTextNode(tdText));
+    tableData2.appendChild(document.createTextNode("Havana House"));
+    tableData3.appendChild(document.createTextNode("9:00 pm"));
+    
+    tableRow.appendChild(tableData);
+    tableRow.appendChild(tableData2);
+    tableRow.appendChild(tableData3);
+    
+    tableBody.appendChild(tableRow);   
+
+  }
+});
+
