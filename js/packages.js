f808c4e7bb27ccd92bf706fd2f0a32fbc99e25bb
iamdjreight <32808968+iamdjreight@users.noreply.github.com>
2018-07-31 21:40:03 -0700

    Update packages.js

--- js/packages.js
+++ js/packages.js
@@ -1,3 +1,6 @@
 document.addEventListener('click',function(e){
   sessionStorage.setItem('package', e.target.id);
 });
+
+// set copyright current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
