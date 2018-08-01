d754c9901762ef4150fb8b962ba3c732d5537510
iamdjreight <32808968+iamdjreight@users.noreply.github.com>
2018-07-31 21:39:12 -0700

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,5 @@
 const package = sessionStorage.getItem('package');
 document.getElementById(package).checked = true;
+
+// set current year copyright
+document.getElementById('currentYear').textContent = new Date().getFullYear();
